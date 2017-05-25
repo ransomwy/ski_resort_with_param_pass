3eb26d7afd49086a96a849eebb17f1bc871d0ce3
Ransom Valley <ransomwy@gmail.com>
2017-05-25 16:08:04 -0600

    first commit

---
+++ ski_resorts.rb
@@ -0,0 +1,19 @@
+
+
+
+
+def makes_list_of_resorts(r1,r2,r3,r4)
+  puts r1
+  puts r2
+  puts r3
+  puts r4
+end
+
+
+makes_list_of_resorts("alta", "snowbird", "jackson","targhee")
+
+def makes_list_of_resorts2(r1, r2, r3, r4)
+  puts r1+" "+r2+" "+r3+" "+r4
+end
+
+makes_list_of_resorts2("deer"+ " "+"valley", "squaw", "snowbasin", "brighton")
